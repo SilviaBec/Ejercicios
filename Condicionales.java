5f3f5c792eb347db91aa36acba5a641e465f2e1e
SilviaBec <silvibecerra681@gmail.com>
2022-09-15 18:18:33 -0500

    Repaso condicionales Java

---
+++ Condicionales.java
@@ -0,0 +1,12 @@
+public class Condicionales {
+    public static void main(String[] args) {
+        int nota = 90;
+    if (nota>= 60 && nota <=80){
+        System.out.println("Bueno");
+    }else if (nota>80){
+        System.out.println("Excelente");
+    }else {
+        System.out.println("Debe reforzar");
+    }
+    }
+}
