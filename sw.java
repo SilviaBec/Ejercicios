88d2cf4037e8a779ece35cc6c5f72685b6018e2a
SilviaBec <silvibecerra681@gmail.com>
2022-09-16 12:53:23 -0500

    Repaso switch en Java

---
+++ sw.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class sw {
+    public static void main(String[] args) {
+        //Mi objeto se llama sc
+    Scanner sc= new Scanner(System.in);
+    System.out.println("Digite el numero de mes (1-4): ");
+    int mes = sc.nextInt();
+    sc.close();
+    String mesLetra ="";
+    switch(mes){
+        case 1:
+        mesLetra ="Enero";
+        break;
+        case 2:
+        mesLetra ="Febrero";
+        break;
+        case 3:
+        mesLetra ="Marzo";
+        break;
+        case 4:
+        mesLetra ="Abril";
+        break;
+        default:
+        System.out.println("Otro mes u otra cosa");
+    }
+    System.out.println("El mes es "+mesLetra
+    );
+        
+    }
+}
