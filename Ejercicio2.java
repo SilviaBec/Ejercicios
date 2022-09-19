949b0bfc7a7a722b8fd963c0efa154559581dd6d
SilviaBec <silvibecerra681@gmail.com>
2022-09-19 09:04:52 -0500

    Ejercicio usando atributos en metodos

---
+++ Ejercicio2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Ejercicio2 {
+    //metodo que captura datos:
+
+    public void capturaDato(){
+        Scanner num= new Scanner(System.in);
+        int tabla;
+        do{
+        System.out.println("Digite la tabla de multiplicar que desea o -1 para finalizar: ");
+        tabla =num.nextInt();
+        if(tabla !=-1){
+            mostrarTabla(tabla);
+        }
+        }while (tabla !=-1);
+    }
+
+    public void mostrarTabla(int valor ){
+        for (int i=1; i<11;i++){
+            System.out.println(valor+"*"+i+"= "+valor*i );
+        }
+    }
+
+    public static void main(String[] args) {
+        Ejercicio2 tabla1 = new Ejercicio2();
+        tabla1.capturaDato();
+    }
+}
