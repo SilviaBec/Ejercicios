872aa105f4c264ef86eaf006335fdeaa8883fbbd
SilviaBec <silvibecerra681@gmail.com>
2022-09-27 18:31:42 -0500

    HERENCIA: Area es CLASE PADRE, rectangulo CLASE HIJA

---
+++ Area.java
@@ -0,0 +1,13 @@
+class Area { //clase padre
+    double dimension1 =10;
+    double dimension2=4;
+    //constructor de mi clase area
+    // public Area(double dimension1, double dimension2){
+    //     this.dimension1=dimension1;
+    //     this.dimension2=dimension2;
+    // }
+    //Pongo un metodo para muestre las dimensiones:
+    public void mostrarDimensiones(){
+        System.out.println("Las dimensiones de la figura son: "+dimension1+" y "+dimension2);
+    }
+}
