c6cb535ab54e13d9375df4a75e2825f788dbc5bf
SilviaBec <silvibecerra681@gmail.com>
2022-09-29 10:59:06 -0500

    HERENCIA: Area es CLASE PADRE, Triangulo CLASE HIJA

---
+++ Triangulo.java
@@ -0,0 +1,17 @@
+class Triangulo extends Area{
+    private String figura ="Triangulo";
+    double area(){
+        return dimension1*dimension2/2;
+    }
+    
+    public static void main(String[] args) {
+        Triangulo obj1 = new Triangulo();
+        System.out.println(obj1.figura);
+        obj1.mostrarDimensiones();
+        System.out.println(obj1.area());
+    }
+
+    
+}
+    
+
