4c5d8f5956090eb1f996497b7d33370026684503
SilviaBec <silvibecerra681@gmail.com>
2022-10-20 22:40:38 -0500

    Ejercicio sencillo conversión de tempratura

---
+++ ejercicioLB2.py
@@ -0,0 +1,7 @@
+# Realizar la conversión de una temperatura dada 
+# en grados Centígrados a grados Fahrenheit 
+# (Fórmula: F = (9/5) C + 32).
+
+centigrados=float(input("Ingrese la temperatura en °Centigrados: "))
+fahrenheit=((9/5)*centigrados)+32
+print(fahrenheit)
