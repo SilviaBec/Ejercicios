7f1a605992f24245d15781c3f42c479a7f8b661a
SilviaBec <silvibecerra681@gmail.com>
2022-10-23 18:39:17 -0500

    Ejercicio sencillo de LB

---
+++ ejercicioLB1.py
@@ -0,0 +1,9 @@
+#Un corredor de maratón (distancia 42,195 Km) ha recorrido la carrera en 2 horas 25 minutos.
+# Se desea un algoritmo que calcule el tiempo medio en minutos por kilómetro.
+distancia=42.194
+horasRecorrido=2
+minRecorrido=25
+
+tTotal= (horasRecorrido*60)+minRecorrido
+tMedio=tTotal/distancia
+print(tMedio)
