5f4d2c6d46f7482bfcb4177dce802bc5dbadb242
SilviaBec <silvibecerra681@gmail.com>
2022-11-02 19:29:58 -0500

    Intento 2 arreglando el orden

--- retoPermut.py
+++ retoPermut.py
@@ -6,7 +6,7 @@ n = 3
 
 i=[]
 j=[]
-k=[]
+k=[] 
 for num in range (x,-1,-1):
     i.append(num)
 for num in range (y,-1,-1):
@@ -14,24 +14,22 @@ for num in range (y,-1,-1):
 for num in range (z,-1,-1):
     k.append(num)
 
-mix=i,j,k
-print(mix)
-perI= itertools.permutations(i)
-perJ= itertools.permutations(j)
-perK= itertools.permutations(k)
+m
+# perJ= itertools.permutations(j)
+# perK= itertools.permutations(k)
 lGrande=[]
 for valor in i:
     lGrande.append(valor)
-for valor in j:
-    lGrande.append(valor)
-for valor in k:
-        lGrande.append(valor)
+# for valor in j:
+#     lGrande.append(valor)
+# for valor in k:
+#         lGrande.append(valor)
 
-per=set(itertools.permutations(lGrande,3))
+per=set(itertools.permutations(lGrande,1))
 
-for val in per:
-    if val[0]+val[1]+val[2]!=n:
-        print(val)
+# for val in per:
+#     if val[0]+val[1]+val[2]!=n:
+#         print(val)
 
 
 #CREO QUE YA , REVISAR COMPARANDO RESUTADOS
