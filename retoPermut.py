3fe9220ae747f4e0fa9057621194e9e430242549
SilviaBec <silvibecerra681@gmail.com>
2022-11-09 22:39:35 -0500

    Nuevo intento sin terminar

--- retoPermut.py
+++ retoPermut.py
@@ -14,24 +14,26 @@ for num in range (y,-1,-1):
 for num in range (z,-1,-1):
     k.append(num)
 
-perI= list(itertools.combinations(i,1))
+lista=[]
+for num in range (x,-1,-1):
+    for num in range (y,-1,-1):
+        for num in range (z,-1,-1):
+            lista.append(num)
+            perK= set(itertools.combinations(lista,3))
+print(perK)
 
-perJ= list(itertools.combinations(j,1))
+# lGrande=[]
+# lista2=[perI,perJ,perK]
+# for valor in i:
+#     lGrande.append(valor)
+# for valor in j:
+#     lGrande.append(valor)
+# for valor in k:
+#         lGrande.append(valor)
 
-perK= set(itertools.combinations(k,1))
+# print(perJ)
 
-lGrande=[]
-lista2=[perI,perJ,perK]
-for valor in i:
-    lGrande.append(valor)
-for valor in j:
-    lGrande.append(valor)
-for valor in k:
-        lGrande.append(valor)
-
-print(perJ)
-
-per=set(itertools.combinations(lGrande,3))
+# per=set(itertools.combinations(lGrande,3))
 
 # for val in per:
 #     if val[0]+val[1]+val[2]!=n:
