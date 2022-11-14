11b6c1b43f247b740137180efb8be17a1a037716
SilviaBec <silvibecerra681@gmail.com>
2022-11-13 20:11:26 -0500

    Finalmente reto terminado en 1 linea, permutaciones, con list comprehension

--- retoPermut.py
+++ retoPermut.py
@@ -1,49 +1,10 @@
-import itertools 
-x = 1
+x = 1 
 y = 1
 z = 2
 n = 3
 
-i=[]
-j=[]
-k=[] 
-for num in range (x,-1,-1):
-    i.append(num)
-for num in range (y,-1,-1):
-    j.append(num)
-for num in range (z,-1,-1):
-    k.append(num)
+print([(i,j,k) for i in range(x+1) for j in range(y+1) for k in range(z+1) if x+y+z !=n])
 
-lista=[]
-for num in k:
-    lista.append(num)
-    for num in i:
-        lista.append(num)
-        for num in j:
-            lista.append(num)
-perK= set(itertools.combinations(lista,3))
-
-for val in perK:
-    if val[0]+val[1]+val[2]!=n:
-            print(val)
-
-# lGrande=[]
-# lista2=[perI,perJ,perK]
-# for valor in i:
-#     lGrande.append(valor)
-# for valor in j:
-#     lGrande.append(valor)
-# for valor in k:
-#         lGrande.append(valor)
-
-# print(perJ)
-
-# per=set(itertools.combinations(lGrande,3))
+                
 
 
-
-
-#CREO QUE YA , REVISAR COMPARANDO RESUTADOS
-
-# for val in perI:
-#     print(val)
